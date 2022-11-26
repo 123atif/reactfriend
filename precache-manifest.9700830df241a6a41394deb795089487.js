494e6ab96f8b408e181b5c68be8a12f6ef99390a
atif <atifarshad0315@gmail.com>
2022-11-26 20:00:24 +0500

    Updates

---
+++ precache-manifest.9700830df241a6a41394deb795089487.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "d17ee7420ffa2a617c51",
+    "url": "/reactfriend/static/css/main.150c8742.chunk.css"
+  },
+  {
+    "revision": "d17ee7420ffa2a617c51",
+    "url": "/reactfriend/static/js/main.d17ee742.chunk.js"
+  },
+  {
+    "revision": "84cf99745ce0980187aa",
+    "url": "/reactfriend/static/js/1.84cf9974.chunk.js"
+  },
+  {
+    "revision": "3a1afea7a2866ce7fc4b",
+    "url": "/reactfriend/static/css/2.035f1502.chunk.css"
+  },
+  {
+    "revision": "3a1afea7a2866ce7fc4b",
+    "url": "/reactfriend/static/js/2.3a1afea7.chunk.js"
+  },
+  {
+    "revision": "71c2ba382fb3c58ee6cb",
+    "url": "/reactfriend/static/js/runtime~main.71c2ba38.js"
+  },
+  {
+    "revision": "86c545ccae4a76a95a9ffdd312221e69",
+    "url": "/reactfriend/static/media/MR ROBOT.86c545cc.woff"
+  },
+  {
+    "revision": "c6883dd9c551bfdea0a9af82b3d4173d",
+    "url": "/reactfriend/index.html"
+  }
+];
